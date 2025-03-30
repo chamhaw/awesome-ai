21b3f5f341bb337c7658d7788b0a259827fde9a0
Chamhaw <93499036+chamhaw@users.noreply.github.com>
2025-03-30 14:48:22 +0800

    chart_type

--- app/cli.py
+++ app/cli.py
@@ -2,6 +2,8 @@ import os
 import time
 from typing import Optional, Dict, Any
 from dashscope.api_entities.dashscope_response import Message
+
+from app import const
 from app.llm import OpenAIInvoker
 from app.tool.sql import execute_sql_to_csv
 from app.tool import extractor
@@ -9,19 +11,13 @@ from app.agent import store
 
 class CLIChat:
     def __init__(self):
-        self.providers = {
-            "1": {"name": "ChatGPT", "base_url": "https://api.openai.com/v1", "env_key": "OPENAI_API_KEY", "models": ["gpt-4o","gpt-4o-mini", "gpt-4-turbo"]},
-            "2": {"name": "Qwen", "base_url": "https://dashscope.aliyuncs.com/compatible-mode/v1", "env_key": "DASHSCOPE_API_KEY", "models": ["qwen-turbo", "qwen-plus", "qwen-max", "qwq-plus"]},
-            "3": {"name": "Ark", "base_url": "https://ark.cn-beijing.volces.com/api/v1", "env_key": "ARK_API_KEY", "models": ["doubao-pro-32k"]},
-            "4": {"name": "DeepSeek", "base_url": "https://api.deepseek.com", "env_key": "DEEPSEEK_API_KEY", "models": ["deepseek-chat", "deepseek-coder", "deepseek-reasoner"]}
-        }
-        self.current_provider = "2"  # 默认使用Qwen
-        self.current_model = "qwen-max"  # 默认模型
+        self.current_provider = "4"  # 默认使用DeepSeek
+        self.current_model = "deepseek-reasoner"  # 默认模型 DeepSeek-R1
         self.history: list[Message] = []
-        self.system_prompt = ""
+        self.system_prompt = ''
+        self.session_id = ''
+        self.csv_path = ''
         self.invoker = self._create_invoker()
-        self.session_id = time.strftime("%Y%m%d%H%M%S", time.localtime())
-        self.csv_path = store.get_csv_path(self.session_id)
         self.db_config = {
             'user': os.getenv('DB_USER'),
             'password': os.getenv('DB_PASSWORD'),
@@ -29,8 +25,15 @@ class CLIChat:
             'database': os.getenv('DB_NAME')
         }
 
+    def reset(self):
+        self.history: list[Message] = []
+        self.system_prompt = ""
+        self.session_id = time.strftime("%Y%m%d%H%M%S", time.localtime())
+        self.csv_path = store.get_csv_path(self.session_id)
+
     def _create_invoker(self) -> OpenAIInvoker:
-        provider = self.providers[self.current_provider]
+        self.reset()
+        provider = const.PROVIDERS[self.current_provider]
         api_key = os.getenv(provider["env_key"])
         if not api_key:
             raise ValueError(f"未设置环境变量 {provider['env_key']}")
@@ -39,7 +42,7 @@ class CLIChat:
             base_url=provider["base_url"]
         )
 
-    def execute_sql(self, sql: str) -> Optional[str]:
+    def execute_sql(self, sql: str) -> str:
         try:
             results = execute_sql_to_csv(sql, self.csv_path, self.db_config)
             if results:
@@ -48,16 +51,17 @@ class CLIChat:
                 return results
             else:
                 print("\n未查到任何统计数据，请重新调整查询条件")
-                return None
+                return ''
         except Exception as e:
             print(f"\nSQL执行错误: {str(e)}")
-            return None
+            return ''
 
-    def process_sql_query(self, user_input: str):
+    def process_sql_query(self, user_input: str) -> tuple[str, str, str, int]:
         # 将时间字符串转换为时间戳进行比较
         session_time = time.mktime(time.strptime(self.session_id, "%Y%m%d%H%M%S"))
         if time.time() - session_time > 1800:  # 30分钟 = 1800秒
             self.session_id = time.strftime("%Y%m%d%H%M%S", time.localtime())
+            self.csv_path = store.get_csv_path(self.session_id)
         system_prompt, user_prompt, history = store.prompt_prepare(
             user_input, self.system_prompt, self.session_id, self.history)
         
@@ -68,54 +72,67 @@ class CLIChat:
             history=history,
             stream=True
         )
-        
         sqls = extractor.extract_sql_queries(response)
+        sql = ''
+        char_type = 1
+        results = ''
         if sqls:
             if len(sqls) > 1:
                 print("\n检测到多个SQL语句，将执行第一个:")
-                for i, sql in enumerate(sqls, 1):
-                    print(f"SQL {i}: {sql}")
-            
-            results = self.execute_sql(sqls[0])
+                for i, _sql in enumerate(sqls, 1):
+                    print(f"SQL {i}: {_sql}")
+            sql = sqls[0]
+            results = self.execute_sql(sql)
             if results:
                 print("\n查询结果:")
                 print(results)
         else:
             print("\nAI回复:")
             print(response)
-        
+        if "图表编号:" in response:
+            # 提取图表编号，并转为数字。数字后面可能有其他字符，要避免和后面的非数字粘连
+            char_type = int([c for c in response.split("图表编号:")[1].split(",")[0] if c.isdigit()][0])
         self.history.append(Message(role='user', content=user_input))
         self.history.append(Message(role='assistant', content=response))
         store.store_context(self.session_id, self.history)
+        return response, sql, results, char_type
 
-    def switch_provider(self):
+    def cli_switch_provider(self):
         print("\n可用的大模型提供商：")
-        for key, provider in self.providers.items():
+        for key, provider in const.PROVIDERS.items():
             print(f"{key}. {provider['name']}({provider['models'][0]})")
         
         choice = input("请选择提供商 (1-4): ").strip()
-        if choice in self.providers:
-            self.current_provider = choice
-            self.current_model = self.providers[choice]["models"][0]
-            self.invoker = self._create_invoker()
-            self.clear_history()
-            print(f"已切换到提供商 {self.providers[choice]['name']}，默认模型：{self.current_model}")
+        if choice in const.PROVIDERS:
+            self.switch_provider(choice)
         else:
             print("无效的选择，保持当前设置")
 
-    def switch_model(self):
-        provider = self.providers[self.current_provider]
+    def switch_provider(self, choice):
+        self.current_provider = choice
+        self.current_model = const.PROVIDERS[choice]["models"][0]
+        self.invoker = self._create_invoker()
+        print(f"已切换到提供商 {const.PROVIDERS[choice]['name']}，默认模型：{self.current_model}")
+
+    def switch_model(self, choice):
+        self.current_model = choice
+        self.reset()
+        print(f"已切换到模型参数：{self.current_model}")
+
+    def cli_switch_model(self):
+        provider = const.PROVIDERS[self.current_provider]
         print(f"\n[{provider['name']}]当前可用的模型:")
         for i, model in enumerate(provider["models"], 1):
             print(f"{i}. {model}")
         
         choice = input("请选择模型参数: ").strip()
         try:
+            if choice in const.PROVIDERS[self.current_provider]["models"]:
+                self.switch_model(choice)
+                return
             idx = int(choice) - 1
             if 0 <= idx < len(provider["models"]):
-                self.current_model = provider["models"][idx]
-                self.clear_history()
-                print(f"已切换到模型参数：{self.current_model}")
+                self.switch_model(provider["models"][idx])
             else:
                 print("无效的选择，保持当前设置")
         except ValueError:
@@ -126,12 +143,11 @@ class CLIChat:
         print("系统提示词已更新")
 
     def clear_history(self):
-        self.history = []
-        self.session_id = time.strftime("%Y%m%d%H%M%S", time.localtime())
+        self.reset()
         print("对话历史已清除")
 
     def chat(self, user_input: str) -> str:
-        print(f"----- {self.providers[self.current_provider]['name']} streaming request with LLM {self.current_model} -----")
+        print(f"----- {const.PROVIDERS[self.current_provider]['name']} streaming request with LLM {self.current_model} -----")
         response = self.invoker.call(
             model=self.current_model,
             system_prompt=self.system_prompt,
@@ -146,15 +162,12 @@ class CLIChat:
     def run(self):
         print("欢迎使用 AI 助手！\n")
         print("可用的大模型提供商：")
-        for key, provider in self.providers.items():
+        for key, provider in const.PROVIDERS.items():
             print(f"{key}. {provider['name']}({provider['models'][0]})")
         
         choice = input("请选择提供商 (1-4): ").strip()
-        if choice in self.providers:
-            self.current_provider = choice
-            self.current_model = self.providers[choice]["models"][0]
-            self.invoker = self._create_invoker()
-            print(f"已切换到提供商 {self.providers[choice]['name']}，默认模型：{self.current_model}")
+        if choice in const.PROVIDERS:
+            self.switch_provider(choice)
         else:
             print("无效的选择，使用默认设置")
 
@@ -168,7 +181,7 @@ class CLIChat:
         print("-" * 40)
 
         while True:
-            provider = self.providers[self.current_provider]
+            provider = const.PROVIDERS[self.current_provider]
             prompt = f"\n[{provider['name']}({self.current_model})]>"
             user_input = input(prompt).strip()
 
@@ -178,10 +191,10 @@ class CLIChat:
                 self.clear_history()
                 continue
             elif user_input.lower() == 'switch':
-                self.switch_provider()
+                self.cli_switch_provider()
                 continue
             elif user_input.lower() == 'model':
-                self.switch_model()
+                self.cli_switch_model()
                 continue
             elif user_input.lower().startswith('system:'):
                 self.set_system_prompt(user_input[7:].strip())
@@ -192,4 +205,5 @@ class CLIChat:
                 continue
 
             if user_input:
-                self.process_sql_query(user_input) 
+                self.process_sql_query(user_input)
+
--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 import time
 from urllib.parse import quote
 
-from dashscope.api_entities.dashscope_response import Message
 from flask import Flask, request, jsonify, make_response, send_file
 from dotenv import load_dotenv
 from flasgger import Swagger, swag_from
 
 from app.agent import store
+from app.cli import CLIChat
 from app.tool.sql import execute_sql_to_csv
 
 flask_app = Flask(__name__)
@@ -23,9 +23,6 @@ flask_app.config['SWAGGER'] = {
     "headers": [],  # 可添加全局请求头（如认证 Token）
 }
 
-from app import llm
-from app.tool import extractor
-
 DB_CONFIG = {
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
@@ -36,47 +33,49 @@ DB_CONFIG = {
 SERVER_URL = os.getenv('SERVER_URL') or 'http://localhost:5050'
 DOWNLOAD_API = "/api/download_file"
 
+# 加载 .env 文件
+load_dotenv()
+chat = CLIChat()
+
 # 新增生成SQL的API端点
 @flask_app.route('/api/chat', methods=['POST'])
 @swag_from('doc/chat.yaml')
 def generate_sql():
     """生成SQL语句
     """
-    session_id = request.cookies.get('x-session-id') or time.strftime("%Y%m%d%H%M%S", time.localtime())
     raw_user_prompt = request.json.get('user_prompt')
-    csv_path = request.json.get('csv_path') or store.get_csv_path(session_id)
-    history = request.json.get('history') or []
-    system_prompt, user_prompt, history = store.prompt_prepare(
-        raw_user_prompt, request.json.get('system_prompt'),
-        session_id,
-        history)
-    response = llm.qwen_qwq_call(system_prompt, user_prompt, history)
-    sqls = extractor.extract_sql_queries(response)
+    provider = request.json.get('provider') or '4'
+    model = request.json.get('model') or 'deepseek-reasoner'
+    clear = request.json.get('clear') or False
+    if clear or not request.cookies.get('x-session-id'):
+        chat.reset()
+    if provider and provider != chat.current_provider:
+        chat.switch_provider(provider)
+    if model and model != chat.current_model:
+        chat.switch_model(model)
+    history = request.json.get('history')
+    if history:
+        chat.history = history
+
+    system_prompt = request.json.get('system_prompt')
+    if system_prompt:
+        chat.set_system_prompt(system_prompt)
+    response, sql, sql_result, chart_type = chat.process_sql_query(raw_user_prompt)
     response_body = {}
-    if not sqls or "需要您补充以下信息" in response:
+    if not sql or "需要您补充以下信息" in response:
         response_body['message'] = response
     else:
-        if len(sqls) > 1:
-            print("有多个SQL语句, 可能需要调整 prompt")
-            for i in range(len(sqls)):
-                print(f'sql: {i + 1}-{sqls[i]}\n')
-        response_body['sql'] = sqls[0]
-        results = execute_sql_to_csv(response_body['sql'], csv_path, DB_CONFIG)
-        if not results:
+        response_body['sql'] = sql
+        if not sql_result:
             response_body['message'] = "未查到任何统计数据，请重新调整查询条件"
         else:
-            response_body['csv'] = results
-            response_body['file_path'] = csv_path
-            response_body['file_link'] = f"{SERVER_URL}/{DOWNLOAD_API}?file_path={quote(csv_path)}"
-
-    history.append(Message(role='user', content=raw_user_prompt))
-    history.append(Message(role='assistant', content=response))
-    response_body['history'] = history
-
-    store.store_context(session_id, history)
+            response_body['chart_type'] = chart_type
+            response_body['csv'] = sql_result
+            response_body['file_path'] = chat.csv_path
+            response_body['file_link'] = f"{SERVER_URL}/{DOWNLOAD_API}?file_path={quote(chat.csv_path)}"
     
     resp = make_response(response_body)
-    resp.set_cookie('x-session-id', session_id,
+    resp.set_cookie('x-session-id', chat.session_id,
                     httponly=True,
                     secure=True,
                     max_age= 1800)
@@ -112,6 +111,4 @@ def download_csv():
 
 # 启动Flask应用
 if __name__ == "__main__":
-    # 加载 .env 文件
-    load_dotenv()
     flask_app.run(host='0.0.0.0', port=5050, debug=True)
